caf06ca0a872fac8c9a6ba0c1cefe86409f3610b
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-22 11:42:44 -0500

    updates

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,43 +1,20 @@
 const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => {
-  
-  if(env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password
-          }, 
-          options: {
-            ssl: false
-          }
-        }
-      }
-    }
-  }
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
     },
-  }
-};
+    debug: false,
+  },
+});
+ 
