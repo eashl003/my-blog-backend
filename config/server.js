c0a1fa9eb30553500134f4f5d6b0cd75a23e5315
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-01-22 16:03:31 -0500

    test

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env("HEROKU_POSTGRESQL_AQUA_URL"),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '58979443c8164474fa1dac0051b0a9ec'),
+    },
+  },
 });
